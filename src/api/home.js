2294218a40896db392a04dba844ae4b4b16a98f2
hongduccodedao <contact.hongduc@gmail.com>
2023-05-29 08:08:07 +0700

    🔧 chore(.env): add VITE_REACT_APP_API_URL environment variable 📦 chore(package.json): add axios dependency 🎉 feat(api): add home api 🎉 feat(api): export home api from index.js 🎉 feat(axios.js): create axios instance with baseURL from environment variable 🚀 feat(Home.jsx): fetch data from home api on component mount

---
+++ src/api/home.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-async-promise-executor */
+import axios from "../axios";
+
+export const getHome = () =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const response = await axios({
+        url: "/home",
+        method: "GET",
+      });
+
+      resolve(response);
+    } catch (error) {
+      reject(error);
+    }
+  });
--- src/containers/public/Home.jsx
+++ src/containers/public/Home.jsx
@@ -1,6 +1,17 @@
 import { Header } from "../../components";
+import * as api from "../../api";
+import { useEffect } from "react";
 
 const Home = () => {
+
+  useEffect(() => {
+    const fetchDataHome = async () => {
+      const response = await api.getHome();
+      console.log(response);
+    }
+    fetchDataHome();
+  }, []);
+
   return (
     <div className="overflow-y-auto">
       <div className="h-[70px] px-[59px] flex items-center">
